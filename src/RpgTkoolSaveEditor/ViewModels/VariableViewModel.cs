3d453e1b855b880353f8d124302df4d653033960
huurou <taihei.kitada@gmail.com>
2025-07-20 10:05:09 +0900

    Variableの値が数字でもTextBoxを経由することでstringとして保存されているっぽいので修正

--- src/RpgTkoolSaveEditor/ViewModels/VariableViewModel.cs
+++ src/RpgTkoolSaveEditor/ViewModels/VariableViewModel.cs
@@ -10,16 +10,20 @@ public partial class VariableViewModel(Variable model) : ObservableObject
     [ObservableProperty]
     public partial object? Value { get; set; } = model.Value;
 
+    partial void OnValueChanged(object? value)
+    {
+        if (value is not null && double.TryParse(value.ToString(), out var number))
+        {
+            Value = number;
+        }
+        WeakReferenceMessenger.Default.Send(new SaveDataChangedMessage());
+    }
+
     public int Id { get; } = model.Id;
     public string Name { get; } = model.Name;
 
     public Variable ToModel()
     {
         return new(Id, Name, Value);
-    }
-
-    partial void OnValueChanged(object? value)
-    {
-        WeakReferenceMessenger.Default.Send(new SaveDataChangedMessage());
     }
 }
